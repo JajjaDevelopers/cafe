d72080a17411f217feec69f100c753f77631d0e9
dev global <kiboolif@gmail.com>
2023-02-16 14:26:26 +0300

    updates

--- home/forms/header.php
+++ home/forms/header.php
@@ -199,7 +199,7 @@ include "../private/verAndApprFunctions.php";
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="logout.php">
+              <a class="dropdown-item d-flex align-items-center" href="../forms/logout.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
