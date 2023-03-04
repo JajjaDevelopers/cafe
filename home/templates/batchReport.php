b27a49a70fd43af7e6b2f4ce9f0a0a2a46b7de73
dev global <kiboolif@gmail.com>
2023-03-04 18:20:44 +0300

    updates

--- home/templates/batchReport.php
+++ home/templates/batchReport.php
@@ -1,4 +1,8 @@
 <h3 id="batchReportHeading" class="formHeading">Production Report</h3>
+<div class=" mt-3 ms-5 d-flex flex-column align-items-start">
+    <i class="bi bi-printer-fill" style="color:green; font-size:30px" id="print">
+    </i>
+</div>
 <?php
     include "../alerts/message.php";
     ?>
